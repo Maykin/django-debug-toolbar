69c5bc4765c9c825094bed6f148ec77648b1286e
Joeri Bekker <joeri@maykinmedia.nl>
2011-06-10 14:41:37 +0200

    Updated auth middleware to new Django path.

--- example/settings.py
+++ example/settings.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
 
 # Required for Django Debug Toolbar
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
 )
