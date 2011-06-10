e00ac8aed237bb8ca296de7956e99d4abdc1884f
Joeri Bekker <joeri@maykinmedia.nl>
2011-06-10 14:33:20 +0200

    Updated example project to comply with Django 1.3 and updated some sane defaults.

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
-from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,6 +13,12 @@ urlpatterns = patterns('',
     (r'^mootools/index/$', direct_to_template, {'template': 'mootools/index.html'}),
     (r'^prototype/index/$', direct_to_template, {'template': 'prototype/index.html'}),
     (r'^admin/', include(admin.site.urls)),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
